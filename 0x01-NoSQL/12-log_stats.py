9f8ac6df3536128e8fc308ff789d0154361871fb
Yuskab <Yuskab021993@gmail.com>
2023-10-18 02:24:54 -0700

    0x01. NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-"""log stats from collection
+"""
+Python script that provides some stats about Nginx logs stored in MongoDB
+Database: logs, Collection: nginx, Display same as example
+first line: x logs, x number of documents in this collection
+second line: Methods
+5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+one line with method=GET, path=/status
 """
 from pymongo import MongoClient
 
@@ -8,7 +14,9 @@ METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    Prototype: def log_stats(mongo_collection, option=None):
+    Provide some stats about Nginx logs stored in MongoDB
     """
     items = {}
     if option:
